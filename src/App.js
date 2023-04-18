5e195a99fd2bee32798283e981e4aae4ba562af6
ParkJaeSeok <qkrwotjr0@gmail.com>
2023-04-18 09:29:21 +0900

    챗봇 오류 재수정

--- src/App.js
+++ src/App.js
@@ -34,6 +34,8 @@ function App() {
       </BrowserRouter>
       <Chat/>
     </div>
+    
+   
   );
 }
 
--- src/pages/chat.js
+++ src/pages/chat.js
@@ -2,22 +2,21 @@ import React,{Component} from "react";
 
 export class chat extends Component {
     
-    componentDidMount(){
-
-    (function(d, m){
-        var kommunicateSettings = 
-            {"appId":"d562b49efa6a5ef8c0cfb2d3fc019fa","popupWidget":true,"automaticChatOpenOnNavigation":true};
-        var s = document.createElement("script1");
-        s.type = "text/javascript";
-         s.async = true;
-        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-        var h = document.getElementsByTagName("head")[0];
-         h.appendChild(s);
-        window.kommunicate = m;
-        m._globals = kommunicateSettings;
-    })(document, window.kommunicate || {});
-
-    }
+    componentDidMount() {
+        if (!window.kommunicate) {
+          (function(d, m){
+            var kommunicateSettings = {"appId":"d562b49efa6a5ef8c0cfb2d3fc019fa","popupWidget":true,"automaticChatOpenOnNavigation":true};
+            var s = document.createElement("script");
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+            var h = document.getElementsByTagName("head")[0];
+            h.appendChild(s);
+            window.kommunicate = m;
+            m._globals = kommunicateSettings;
+          })(document, window.kommunicate || {});
+        }
+      }
     render() {
         return(
             <div>
