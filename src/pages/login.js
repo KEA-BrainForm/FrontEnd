977a9ae558f0d1a25049d4398b9842b3db20e8bf
rkdejr2321 <rkdejr2321@naver.com>
2023-05-01 23:23:58 +0900

    [Feat] 소셜 로그인 및 JWT 토큰 저장

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import './css/home.css'
 import logo from '../images/Logo.png';
-import googleLogin from '../images/google_login.png';
+import googleLoginImg from '../images/google_login.png';
 import kakaoLogin from '../images/kakao_login.png';
 import naverLogin from '../images/naver_login.png';
-import Chat from './chat';
 
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+import { Link } from 'react-router-dom';
+
 function ImageButton(props) {
     return (
         <Link to={props.link}>
         <button onClick={props.onClick}>
             <img src={props.src} alt={props.altText} width="300px" height="50px" />
-            
         </button>
         </Link>
     );
 }
-
 
 
 const login = () => {
@@ -33,14 +32,9 @@ const login = () => {
                     <h1>Log in</h1>
                     <h5>회원가입을 통해 더 많은 서비스를 즐겨보세요</h5> <br/>
 
-                    <ImageButton src={googleLogin} alt="구글 로그인" link="/signup"/><br />  {/* link 속성에 이동할 페이지 url 입력하면 됩니다 -> 회원가입 페이지 */} 
-                    <ImageButton src={kakaoLogin} alt="카카오 로그인" link="/signup"/><br /> { /* " */}
-                    <ImageButton src={naverLogin} alt="네이버 로그인" link="/signup"/><br />  { /* " */}
-
-                </div>
-                <div>
-                    <br /><br />
-                    <h5>Kakao 99%, All rights reserved</h5>
+                    <ImageButton src={googleLoginImg} alt="구글 로그인" link="http://localhost:8080/oauth2/authorization/google"/><br />  {/* link 속성에 이동할 페이지 url 입력하면 됩니다 -> 회원가입 페이지 */} 
+                    <ImageButton src={kakaoLogin} alt="카카오 로그인" link="http://localhost:8080/oauth2/authorization/kakao"/><br /> { /* " */}
+                    <ImageButton src={naverLogin} alt="네이버 로그인" link="http://localhost:8080/oauth2/authorization/naver"/><br />  { /* " */}
                 </div>
            
             </div>
