f70e0f197466630b34638ca0c9a76967d49da5f6
ParkJaeSeok <qkrwotjr0@gmail.com>
2023-05-10 11:01:21 +0900

    버튼 css 수정

--- src/pages/ui/Button.jsx
+++ src/pages/ui/Button.jsx
@@ -2,17 +2,26 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-    padding: 8px 16px;
-    font-size: 16px;
-    border-width: 1px;
-    border-radius: 8px;
-    cursor: pointer;
+margin-top: 0rem;
+background-color: #363cf4;
+color: #ffffff;
+border: none;
+padding: 0.5rem 1rem;
+font-size: 1rem;
+cursor: pointer;
+transition: background-color 0.3s ease;
+border-radius: 4px; /* Make the button round */
+
 `;
+
+
+  
+
 
 function Button(props) {
     const { title, onClick } = props;
-
-    return <StyledButton onClick={onClick}>{title || "button"}</StyledButton>;
+    
+    return <StyledButton onClick={onClick}>{title || "Submit"}</StyledButton>;
 }
 
 export default Button;
