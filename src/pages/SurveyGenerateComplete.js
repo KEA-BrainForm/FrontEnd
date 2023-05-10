11767529d5c587dbb7193b5096373a0851300312
Haebin AHN <103311941+HaebinAHN@users.noreply.github.com>
2023-05-10 18:40:33 +0900

    url 클립보드 복사버튼 활성화

--- src/pages/SurveyGenerateComplete.js
+++ src/pages/SurveyGenerateComplete.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import logo from "../images/Logo.png";
 import kakaoShare from "../images/kakao_share.png";
 import urlShare from "../images/url_share.png";
+import CopyToClipboard from "react-copy-to-clipboard";
 
-import './css/surveycomplete.css'
-import brainwaves from '../images/brainwaves.png';
-import { Link } from 'react-router-dom';
+import "./css/surveycomplete.css";
+import brainwaves from "../images/brainwaves.png";
+import { Link } from "react-router-dom";
 import { Grid, TextField } from "@mui/material";
 import { useLocation } from "react-router-dom";
 
@@ -13,16 +14,24 @@ function SurveyGenerateComplete() {
   const location = useLocation();
   const uniqueUrl = location.state.uniqueUrl;
   console.log(uniqueUrl);
-  
-  return (
 
-    <div className='background'>
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  return (
+    <div className="background">
       <img src={logo} alt="Logo" className="img" />
       <div className="home-card">
         <div className="complete-card">
           <div class="container2">
             <div class="item2 itemmerge">
-              <h2><strong>설문 생성이 완료되었습니다.</strong></h2>
+              <h2>
+                <strong>설문 생성이 완료되었습니다.</strong>
+              </h2>
             </div>
             <div class="item2 itemmerge2">
               <h2>설문 내용을 공유해보세요.</h2>
@@ -33,16 +42,30 @@ function SurveyGenerateComplete() {
                 label="URL"
                 variant="outlined"
                 value={uniqueUrl}
-                style={{background: 'white', width: '50%'}}
+                style={{ background: "white", width: "50%" }}
                 InputProps={{
-                  style: { color: 'black', textAlign: 'center' }
+                  style: { color: "black", textAlign: "center" },
                 }}
               />
-              <img src={urlShare} width='70px' height='auto' style={{ marginLeft: "10px" }} />
+              <CopyToClipboard text={uniqueUrl} onCopy={handleCopy}>
+                <img
+                  src={urlShare}
+                  width="70px"
+                  height="auto"
+                  style={{ marginLeft: "10px", cursor: "pointer" }}
+                />
+              </CopyToClipboard>
             </div>
+            {copied ? (
+              <div style={{ color: "red" }}>Copied to clipboard</div>
+            ) : null}
           </div>
           <div class="con2">
-              <img src={kakaoShare} class="shareImg" style={{ marginBottom: "50px" }}/>
+            <img
+              src={kakaoShare}
+              class="shareImg"
+              style={{ marginBottom: "50px" }}
+            />
           </div>
           <Link to="/">
             <button className="completebutton">닫기</button>
@@ -52,6 +75,5 @@ function SurveyGenerateComplete() {
     </div>
   );
 }
-
 
 export default SurveyGenerateComplete;
