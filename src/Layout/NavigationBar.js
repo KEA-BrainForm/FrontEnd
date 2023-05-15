450a2b3132da08eb7f7929f155ed675c99130ee3
manips <jwjw6410@gmail.com>
2023-05-15 10:30:43 +0900

    프론트 수정

--- src/Layout/NavigationBar.js
+++ src/Layout/NavigationBar.js
@@ -3,18 +3,28 @@ import Navbar from 'react-bootstrap/Navbar';
 import logo from '../images/Logo.png'
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
-
+import React, { useState } from 'react';
 function NavigationBar() {
+
+  const [hovered, setHovered] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHovered(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(null);
+  };
   return (
     <>
-    <Navbar
-  bg="white"
-  variant="blue"
-  style={{
-    height: '50px',
-    borderBottom: '1px solid #ADD8E6',
-  }}
->
+      <Navbar
+        bg="white"
+        variant="blue"
+        style={{
+          height: '50px',
+          borderBottom: '1px solid #ADD8E6',
+        }}
+      >
         <Container>
           <Navbar.Brand as={Link} to="/">
             <img
@@ -27,10 +37,31 @@ function NavigationBar() {
             BrainForm
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/createsurvey">설문 생성</Nav.Link>
-            <Nav.Link as={Link} to="/managesurvey">설문 관리</Nav.Link>
-            <Nav.Link as={Link} to="/mypage">My Page</Nav.Link>
-            <Nav.Link as={Link} to="/login">Log In</Nav.Link>
+            <Nav.Link as={Link} to="/createsurvey"
+              onMouseEnter={() => handleMouseEnter(0)}
+              onMouseLeave={handleMouseLeave}
+              style={{
+                color: hovered === 0 ? 'blue' : 'inherit',
+              }}>설문 생성</Nav.Link>
+            <Nav.Link as={Link} to="/managesurvey"
+              onMouseEnter={() => handleMouseEnter(1)}
+              onMouseLeave={handleMouseLeave}
+              style={{
+                color: hovered === 1 ? 'blue' : 'inherit',
+              }}>설문 관리</Nav.Link>
+            <Nav.Link as={Link} to="/mypage"
+              onMouseEnter={() => handleMouseEnter(2)}
+              onMouseLeave={handleMouseLeave}
+              style={{
+                color: hovered === 2 ? 'blue' : 'inherit',
+              }}>My Page</Nav.Link>
+          </Nav>
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/login" onMouseEnter={() => handleMouseEnter(3)}
+              onMouseLeave={handleMouseLeave}
+              style={{
+                color: hovered === 3 ? 'blue' : 'inherit',
+              }}>Log In</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
