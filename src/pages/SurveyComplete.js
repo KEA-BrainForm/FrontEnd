b40d0f3bf0ab8ad6e3b7e5b316c216564cc6b1d2
Haebin AHN <103311941+HaebinAHN@users.noreply.github.com>
2023-06-04 17:42:59 +0900

    modified logo

--- src/pages/SurveyComplete.js
+++ src/pages/SurveyComplete.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from '../images/Logo.png';
+import logo from '../images/Logo2.png';
 
 import './css/surveycomplete.css'
 import brainwaves from '../images/brainwaves.png';
